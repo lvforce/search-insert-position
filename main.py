e76ee8b1c84b54c4d6f382c73e6ab717a4b78082
rp_prodan <one.prodan1@gmail.com>
2020-03-22 22:33:41 +0200

    compared perfomance of recursive and non-recursive version

--- main.py
+++ main.py
@@ -17,12 +17,18 @@ Input: [1,3,5,6], 0
 Output: 0
 """
 
+import random
+import time
+
+
 def binary_search(arr, left, right, value):
+	start_time = time.time()
 	if right >= left:
 
 		mid = left + (right - left) // 2
 		if arr[mid] == value:
-			return mid
+			print("Time of execution recursive: %f s" % (time.time() - start_time))
+			return mid 
 		elif arr[mid] > value:
 			return binary_search(arr, left, mid - 1, value)
 		else:
@@ -30,13 +36,55 @@ def binary_search(arr, left, right, value):
 	else:
 		for i in range(len(arr)):
 			if value < arr[i]:
+				print("Time of execution recursive:: %f s" % (time.time() - start_time))
 				return i
-		return len(arr)		
-	
+		print("Time of execution recursive:: %f s" % (time.time() - start_time))		
+		return len(arr)
+
+
+def iterative_binary_search(arr, left, right, value):
+	start_time = time.time()
+	while left <= right:
+
+		mid = left + (right - left) // 2
+		if arr[mid] == value:
+			print("Time of execution iterative: %f s" % (time.time() - start_time))
+			return mid
+		elif arr[mid] < value:
+			left = mid + 1
+		else:
+			right = mid - 1 
+	else:
+		for i in range(len(arr)):
+			if value < arr[i]:
+				print("Time of execution iterative: %f s" % (time.time() - start_time))
+				return i
+		print("Time of execution iterative: %f s" % (time.time() - start_time))
+		return len(arr)
 
 
 
+def for_hundred_thousand_items():
+	print("Search for hundred thousand items")
+	arr = [random.randint(0,100000) for _ in range(100000)]
+	arr.sort()
+	test = [1,3,5,6]
+	x = iterative_binary_search(arr, 0, len(arr)-1, 20123)
+	print("iterative position: ", x)
+	print('')
+	y = binary_search(arr, 0, len(arr)-1, 20123)
+	print("recursive position: ", y)
 
-arr = [1,3,5,6]
-x = binary_search(arr, 0, len(arr)-1, 7)
-print(x)
+def for_one_million_items():
+	print("Search for one million items")
+	arr = [random.randint(0,1000000) for _ in range(1000000)]
+	arr.sort()
+	x = iterative_binary_search(arr, 0, len(arr)-1, 420123)
+	print("iterative position: ", x)
+	print('\n')
+	y = binary_search(arr, 0, len(arr)-1, 420123)
+	print("recursive position: ", y)
+
+for_hundred_thousand_items()
+print("=============================")
+for_one_million_items()
